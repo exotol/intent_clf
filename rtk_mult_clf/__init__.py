a5f4bb24086bdee45d134037cbe93f39fd83a10b
exotol <artem.bardakov@gmail.com>
2022-06-14 15:31:28 +0300

    Experiment log_reg_multiclass_tfidf_default, target score: 0.8531598513011153

--- rtk_mult_clf/__init__.py
+++ rtk_mult_clf/__init__.py
@@ -2,4 +2,8 @@ __version__ = "0.1.0"
 
 
 from .datamodules.datamodule_sklearn import SklearnRTKDataModule
-from .features.transformers import CountVectorizerDF, make_pipeline
+from .features.transformers import (
+    CountVectorizerDF,
+    TfIdfVectorizerDF,
+    make_pipeline
+)
