3f1bbb3e3fecd0f2bc91351df8a3f1d4bdfd15ab
exotol <artem.bardakov@gmail.com>
2022-06-14 14:13:36 +0300

    добавил скрипты для запуска экспериментов и предсказаний

---
+++ scripts/run_experiment.cmd
@@ -0,0 +1,1 @@
+python tasks/train_sklearn_model.py --config-dir F:\shad\projects\pet\rtk_mult_clf\configs\sklearn experiment=example ~model.multi_class
---
+++ scripts/run_predict_sklearn_model.cmd
@@ -0,0 +1,1 @@
+python tasks/predict_sklearn_model.py --config-dir F:\shad\projects\pet\rtk_mult_clf\configs\sklearn
