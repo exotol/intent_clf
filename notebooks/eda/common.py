49a998f736a9959614163823d4cc58b88d15f973
exotol <artem.bardakov@gmail.com>
2022-06-14 13:51:00 +0300

    Переупорядочен импорт

--- notebooks/eda/common.py
+++ notebooks/eda/common.py
@@ -1,19 +1,20 @@
 import os
-import pandas as pd
-import numpy as np
-import seaborn as sns
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from tqdm import tqdm
+from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
 
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.max_rows', 50)
-pd.set_option('display.max_colwidth', None)
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.max_rows", 50)
+pd.set_option("display.max_colwidth", None)
 
 
-PROJECT_PATH_ROOT = os.getenv('PROJECT_PATH_ROOT')
-SEED = int(os.getenv('SEED'))
+PROJECT_PATH_ROOT = os.getenv("PROJECT_PATH_ROOT")
+SEED = int(os.getenv("SEED"))
 
 DATA_PATH = os.path.join(PROJECT_PATH_ROOT, "data", "raw")
 
